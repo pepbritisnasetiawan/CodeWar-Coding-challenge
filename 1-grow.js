e3b6274593f7be7bc48da9c56b5867e4ff68a24f
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-01-23 21:51:37 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,6 @@
+// WPU Coding Challenge 2024
+// 1/366
+
 /* function grow(x) {
     let result = x[0];
     for (let i = 1; i < x.length; i++) {
@@ -8,7 +11,7 @@
 
 console.log(grow([1,2,3,4])); */
 
-// reduce menerima
+// reduce mengembalikan array baru,
 
 const grow = (x) =>
   x.reduce((accumulator, currentValue) => accumulator * currentValue);
