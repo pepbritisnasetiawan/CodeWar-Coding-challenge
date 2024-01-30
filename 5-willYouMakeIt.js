ddff66939e2c118f475f390efa9457dfd111c64b
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-01-30 23:36:30 +0700

    update description

--- 5-willYouMakeIt.js
+++ 5-willYouMakeIt.js
@@ -1,6 +1,6 @@
 /* 
 WPU Coding Challenge 2024
-1/366
+5/366
 
 You were camping with your friends far away from home, but when it's time to go back,
 you realize that your fuel is running out and the nearest pump is 50 miles away!
