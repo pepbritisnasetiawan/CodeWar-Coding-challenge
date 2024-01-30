0fe898784f22f1293a8f71c3b3ac28feab4e9973
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-01-31 00:17:26 +0700

    CodeWars coding challenge day 6/366

--- 6-dnaToRna.js
+++ 6-dnaToRna.js
@@ -1,7 +1,13 @@
 /* 
-Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+WPU Coding Challenge 2024
+6/366
 
-Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems.
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells.
+RNA differs slightly from DNA its chemical structure and contains no Thymine.
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
 
 Create a function which translates a given DNA string into RNA.
 
@@ -9,7 +15,33 @@ For example:
 
 "GCAT"  =>  "GCAU"
 
-The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+The input string can be of arbitrary length - in particular, it may be empty.
+All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 Fundamentals
 Strings
  */
+
+// function DNAtoRNA(dna) {
+//   // create a function which returns an RNA sequence from the given DNA sequence
+//   let rna = '';
+//   for (let i = 0; i < dna.length; i++) {
+//     /* if (dna[i] === 'T') {
+//         rna += 'U';
+//     } else {
+//         rna += dna[i];
+//     } */
+//     dna[i] === 'T' ? rna+= 'U' : rna += dna[i];
+//   }
+//   return rna;
+// }
+
+/* const DNAtoRNA = (dna) =>
+  dna
+    .split('')
+    .map((el) => (el === 'T' ? 'U' : el))
+    .join('');
+ */
+
+const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+console.log(DNAtoRNA('TTT'));
